ea41f72f035b022be422cee38ed16df63772c4cd
Semen Simanov <63959715+bpatapb88@users.noreply.github.com>
2020-04-19 21:21:21 +0200

    First kata done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
-
+import java.util.regex.*;
+// Responsible Drinking Kata
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String str = "1 beer, 3 vines";
+        String pattern = "(\\d)";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(str);
+        int result = 0;
+        while (m.find()) {
+            result += Integer.parseInt(m.group(0));
+        }
+        System.out.println(result);
     }
 }
